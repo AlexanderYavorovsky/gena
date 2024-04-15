6a74cad3224fdc40b371e15737d306c1ddc049f4
LeonidLodygin <st108157@student.spbu.ru>
2024-04-15 21:03:14 +0300

    Upgrade muta

--- muta.py
+++ muta.py
@@ -1,16 +1,26 @@
 import random
 import copy
+import subprocess
+import os
+import argparse
+
+csmith_dir = 'csmith'
+gen_dir = 'generated'
+gen_filename = 'my1.c'
+out_filename = 'my1.out'
+
+
 
 template_config = {
     '--seed': range(1, 100),
-    '--max-funcs': range(1, 600),
-    '--max-expr-complexity': range(2, 500)
+    '--max-funcs': range(1, 10),
+    '--max-expr-complexity': range(2, 50)
 }
 
 def generate_config():
     config = dict()
     for flag in template_config.keys():
-        config[flag] = random.choice(template_config[flag])
+        config[flag] = str(random.choice(template_config[flag]))
     return config
 
 def crossover_func(chrom1, chrom2):
@@ -23,11 +33,16 @@ def crossover_func(chrom1, chrom2):
 def mutation_func(chromosome):
     child = chromosome.copy()
     random_key = random.choice(list(chromosome.keys()))
-    child[random_key] = random.choice(template_config[random_key])
+    child[random_key] = str(random.choice(template_config[random_key]))
     return child
 
 def fitness_func(chromosome):
-    result = sum(chromosome.values())
+    generate(gen_filename, chromosome)
+    build(gen_filename, out_filename)
+    print(f'Generated file {os.path.join(gen_dir, gen_filename)}'
+      f' and built it into {os.path.join(gen_dir, out_filename)}')
+      #
+    result = random.randint(1, 100)
     if result != desired_output:
         fitness = 1.0 / abs(result - desired_output)
     else:
@@ -53,11 +68,36 @@ def crossover(population, amount):
         population.append((child2, 0))
 
 
-desired_output = 541
+desired_output = 10
 initial_population = []
 for _ in range(10): 
     chromosome = generate_config()
     initial_population.append((chromosome, 0))
+    
+
+def generate(dest_name, config):
+    os.chdir('..')
+    if not os.path.isdir(gen_dir):
+        os.mkdir(gen_dir)
+    os.chdir(gen_dir)
+
+    # TODO: arguments from mutation
+    run_process = ['/usr/local/bin/csmith']
+    for flag in config.keys():
+        run_process.append(flag)
+        run_process.append(config[flag])
+    gen_proc = subprocess.run(run_process, capture_output=True, text=True)
+
+    with open(dest_name, 'w') as f:
+        f.write(gen_proc.stdout)
+
+
+def build(src_name, dest_name):
+    # TODO: use unknown elf instead
+    subprocess.run(
+        ['riscv64-linux-gnu-gcc', '--static', '-O0',
+         '-I/usr/local/include', src_name, '-o', dest_name],
+        capture_output=True)    
 
 def genetic_func(population, iterations):
     
@@ -77,5 +117,5 @@ def genetic_func(population, iterations):
             population[-5 + index] = (item[0], result)
     return population   
 
-best = genetic_func(initial_population, 10000)
+best = genetic_func(initial_population, 10)
 print(best)
